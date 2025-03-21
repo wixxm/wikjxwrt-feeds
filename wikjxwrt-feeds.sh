8dde366e74ce9b94435ba30ab6dc5e97f8bff820
wixxm <94870224+wixxm@users.noreply.github.com>
2025-03-21 17:24:32 +0800

    Update wikjxwrt-feeds.sh

--- wikjxwrt-feeds.sh
+++ wikjxwrt-feeds.sh
@@ -77,3 +77,6 @@ sync_repo_folder "https://github.com/sirpdboy/luci-app-ddns-go.git" "main" "luci
 sync_repo_folder "https://github.com/gdy666/luci-app-lucky.git" "main" "luci-app-lucky" "luci-app-lucky"
 sync_repo_folder "https://github.com/gdy666/luci-app-lucky.git" "main" "lucky" "lucky"
 sync_repo_folder "https://github.com/wixxm/wikjxwrt-app.git" "main" "luci-app-fileassistant" "luci-app-fileassistant"
+
+# 生成 packages.txt，列出所有同步的目录
+ls -d */ | xargs -n 1 basename | paste -sd ' ' - > packages.txt
